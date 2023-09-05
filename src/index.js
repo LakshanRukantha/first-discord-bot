3ff3c2b7e5a60b9166bf00c5e859e6cc147eecbe
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-09-06 01:57:01 +0530

    feat: Added Discord Bot Integration And Configuration Settings.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-const logger = require("./utils/logger.js")
+require("dotenv").config();
+const logger = require("./utils/logger.js");
+const { Client, IntentsBitField } = require("discord.js");
 
-logger.info("Hello");
+const bot_token = process.env.BOT_TOKEN;
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  logger.info(`${c.user.tag} is live!`);
+});
+
+client.login(bot_token);
